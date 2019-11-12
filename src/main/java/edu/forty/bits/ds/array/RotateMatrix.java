c2a08eb045f0c83b9c40b11da73440770d3ba5a1
Naman Nigam <namannigam12@gmail.com>
2019-11-12 10:44:18 +0530

    Rotate matrix in quadratic time.

--- src/main/java/edu/forty/bits/ds/array/RotateMatrix.java
+++ src/main/java/edu/forty/bits/ds/array/RotateMatrix.java
@@ -2,35 +2,37 @@ package edu.forty.bits.ds.array;
 
 import java.util.Arrays;
 
-// rotate a matrix of NxN   (possible to do this in place?)
+/**
+ * Given an NxN matrix, where each pixel in the image is 4 bytes. Write a method to rotate the image
+ * by 90 degrees. Can this be achieved in place?
+ */
 public class RotateMatrix {
 
-  // one way is to rotate each edge while copying the top first and then swapping, but this requires
-  // O(N) extra space
-  // another is to swap index by index
-  // both the solutions are O(n^2) and can't get better
-  private static void rotateMatrix(int[][] matrix) {
-    if (matrix.length == 0 || matrix.length != matrix[0].length) return;
+  // One way is to rotate each edge while copying the top first and then swapping.
+  // But this requires O(N) extra space
+  // Another way is to swap index by index
+  // Both the solutions are O(n^2) and can't get better.
+  void rotateMatrix(int[][] matrix) {
+    // has to be a square matrix
+    if (matrix.length <= 1 || matrix.length != matrix[0].length) return;
     int n = matrix.length;
     for (int layer = 0; layer < n / 2; layer++) {
       int last = n - 1 - layer;
       for (int i = layer; i < last; i++) {
-        int offset = i - layer;
         int top = matrix[layer][i]; // save top
-
+        int updatedIndex = last - (i - layer);// i-layer is the offset
         // left to top
-        matrix[layer][i] = matrix[last - offset][layer];
+        matrix[layer][i] = matrix[updatedIndex][layer];
 
         // bottom to left
-        matrix[last - offset][layer] = matrix[last][last - offset];
+        matrix[updatedIndex][layer] = matrix[last][updatedIndex];
 
         // right to bottom
-        matrix[last][last - offset] = matrix[i][last];
+        matrix[last][updatedIndex] = matrix[i][last];
 
         // top to right
         matrix[i][last] = top;
       }
     }
-    System.out.println(Arrays.deepToString(matrix));
   }
 }
