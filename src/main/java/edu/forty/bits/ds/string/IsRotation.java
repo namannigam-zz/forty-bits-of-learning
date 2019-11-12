50758ae0a9cfd808fb0dae6a8cb2799e99ae7f8d
Naman Nigam <namannigam12@gmail.com>
2019-11-12 11:21:03 +0530

    String Rotation using substring check.

--- src/main/java/edu/forty/bits/ds/string/IsRotation.java
+++ src/main/java/edu/forty/bits/ds/string/IsRotation.java
@@ -1,15 +1,17 @@
 package edu.forty.bits.ds.string;
 
-// `isSubstring` checks is one string is a substring of another, for
-// s1 and s2,
-// write code to check is s2 is a rotation of s1 by calling the given method only once
+/**
+ * Assume you have a method isSubstring which checks if one word is a substring of another. Given
+ * two strings s1 and s2, write code to check if s2 is a rotation of s1 using only one call to
+ * usSubstring (e.g. "waterbottle" is a rotation of "erbottlewat")
+ */
 public class IsRotation {
 
-  // xy rotates to yx
-  // to make sure a string yx is a rotation of xy, you can confirm if
-  // yx is a substring of x'yx'y
-  private static boolean isRotation(String s1, String s2) {
+  // Logically 'xy' rotates to 'yx' and to make sure a string yx is a rotation of xy,
+  // you can confirm id 'yx' is a substring of "x'yx'y"
+  boolean isRotation(String s1, String s2) {
     String temp = s1 + s1;
     return IsSubstring.isSubstring(temp, s2) != -1;
+    //    return IsSubstring.isSubstringUsingIndexOf(temp, s2);
   }
 }
