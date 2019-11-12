957dbbd748f1f473debc45fab3bf3e72b4724351
Naman Nigam <namannigam12@gmail.com>
2019-11-12 11:05:25 +0530

    Set zero matrix

--- src/main/java/edu/forty/bits/ds/array/ZeroMatrix.java
+++ src/main/java/edu/forty/bits/ds/array/ZeroMatrix.java
@@ -1,17 +1,23 @@
 package edu.forty.bits.ds.array;
 
-// if an element in a MxN matrix is zero, its entire column and row are set to be zero
+/**
+ * Write an algorithm such that if an element in a MxN matrix is zero, its entire column and row are
+ * set to be zero.
+ */
 public class ZeroMatrix {
 
-  private void setZerosInMatrix(int[][] matrix) {
+  // traverse the first row and first column to store their states
+  // while traversing the entire matrix, make the element of first row and column 0 accordingly
+  // traverse first row and column again to nullify
+  // nullify the first row and first column based on the flags set fom first iteration
+  void setZerosInMatrix(int[][] matrix) {
     boolean rowHasZero = false;
     boolean columnHasZero = false;
-
     for (int i = 0; i < matrix[0].length; i++) {
       if (matrix[0][i] == 0) {
         rowHasZero = true;
+        break;
       }
-      break;
     }
 
     for (int j = 0; j < matrix.length; j++) {
@@ -21,6 +27,7 @@ public class ZeroMatrix {
       }
     }
 
+    // set the first element of the row and column as 0 for every such occurrence
     for (int i = 1; i < matrix.length; i++) {
       for (int j = 0; j < matrix[0].length; j++) {
         if (matrix[i][j] == 0) {
@@ -29,6 +36,7 @@ public class ZeroMatrix {
         }
       }
     }
+
     for (int i = 1; i < matrix.length; i++) {
       if (matrix[i][0] == 0) {
         nullifyRow(matrix, i);
@@ -39,14 +47,21 @@ public class ZeroMatrix {
       if (matrix[0][j] == 0) {
         nullifyColumn(matrix, j);
       }
-      break;
     }
 
     if (rowHasZero) nullifyRow(matrix, 0);
     if (columnHasZero) nullifyColumn(matrix, 0);
   }
 
-  private void nullifyRow(int[][] matrix, int i) {}
+  private void nullifyRow(int[][] matrix, int row) {
+    for (int j = 0; j < matrix[0].length; j++) {
+      matrix[row][j] = 0;
+    }
+  }
 
-  private void nullifyColumn(int[][] matrix, int i) {}
+  private void nullifyColumn(int[][] matrix, int col) {
+    for (int i = 0; i < matrix.length; i++) {
+      matrix[i][col] = 0;
+    }
+  }
 }
