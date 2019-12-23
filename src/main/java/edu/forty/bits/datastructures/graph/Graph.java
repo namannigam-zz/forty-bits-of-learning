99d59be9b917de9f5343c57e2caf91e8f282f158
Naman Nigam <namannigam12@gmail.com>
2019-12-23 12:30:00 +0530

    route between nodes

--- src/main/java/edu/forty/bits/datastructures/graph/Graph.java
+++ src/main/java/edu/forty/bits/datastructures/graph/Graph.java
@@ -2,6 +2,8 @@ package edu.forty.bits.datastructures.graph;
 
 public class Graph {
     GraphNode[] nodes;
+    public static int MAX_VERTICES = 6;
+    public int count;
 
     public GraphNode[] getNodes() {
         return nodes;
@@ -10,6 +12,38 @@ public class Graph {
     static class GraphNode {
         int data;
         State state;
-        GraphNode[] adjacents;
+        GraphNode[] adjacent;
+        public int adjacentCount;
+        private String vertex;
+
+        public GraphNode(String vertex, int adjacentLength) {
+            this.vertex = vertex;
+            adjacentCount = 0;
+            adjacent = new GraphNode[adjacentLength];
+        }
+
+        public void addAdjacent(GraphNode x) {
+            if (adjacentCount < adjacent.length) {
+                this.adjacent[adjacentCount] = x;
+                adjacentCount++;
+            } else {
+                System.out.print("No more adjacent can be added");
+            }
+        }
+    }
+
+    public Graph() {
+        nodes = new GraphNode[MAX_VERTICES];
+        count = 0;
+
+    }
+
+    public void addNode(GraphNode x) {
+        if (count < nodes.length) {
+            nodes[count] = x;
+            count++;
+        } else {
+            System.out.print("Graph full");
+        }
     }
 }
--- src/main/java/edu/forty/bits/datastructures/graph/RouteBetweenNodes.java
+++ src/main/java/edu/forty/bits/datastructures/graph/RouteBetweenNodes.java
@@ -10,7 +10,7 @@ public class RouteBetweenNodes {
     // The problem is to perform more of a BFS on the graph, though even DFS works in this case
     // with O(N) runtime complexity. Marking nodes as visited would be the primary responsibility
     // while traversing to reduce repeated traversal.
-    private boolean searchRoute(Graph graph, Graph.GraphNode start, Graph.GraphNode end) {
+    private static boolean searchRoute(Graph graph, Graph.GraphNode start, Graph.GraphNode end) {
         if (start == end) {
             return true;
         }
@@ -25,7 +25,7 @@ public class RouteBetweenNodes {
         while (!list.isEmpty()) {
             node = list.removeFirst(); // dequeue
             if (node != null) {
-                for (Graph.GraphNode graphNode : node.adjacents) {
+                for (Graph.GraphNode graphNode : node.adjacent) {
                     if (graphNode.state == State.UNVISITED) {
                         if (graphNode == end) {
                             return true;
@@ -39,5 +39,36 @@ public class RouteBetweenNodes {
             }
         }
         return false;
+    }
+
+
+    public static void main(String[] args) {
+        Graph g = createNewGraph();
+        Graph.GraphNode[] n = g.getNodes();
+        Graph.GraphNode start = n[3];
+        Graph.GraphNode end = n[5];
+        System.out.println(searchRoute(g, start, end));
+    }
+
+    public static Graph createNewGraph() {
+        Graph g = new Graph();
+        Graph.GraphNode[] temp = new Graph.GraphNode[6];
+
+        temp[0] = new Graph.GraphNode("a", 3);
+        temp[1] = new Graph.GraphNode("b", 0);
+        temp[2] = new Graph.GraphNode("c", 0);
+        temp[3] = new Graph.GraphNode("d", 1);
+        temp[4] = new Graph.GraphNode("e", 1);
+        temp[5] = new Graph.GraphNode("f", 0);
+
+        temp[0].addAdjacent(temp[1]);
+        temp[0].addAdjacent(temp[2]);
+        temp[0].addAdjacent(temp[3]);
+        temp[3].addAdjacent(temp[4]);
+        temp[4].addAdjacent(temp[5]);
+        for (int i = 0; i < 6; i++) {
+            g.addNode(temp[i]);
+        }
+        return g;
     }
 }
