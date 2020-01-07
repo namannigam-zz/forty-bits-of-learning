e83c81ba29b38ffa693a4e4b24efa5234a6abfb3
Naman Nigam <namannigam12@gmail.com>
2020-01-07 12:23:44 +0530

    Baby names graph based.

--- src/main/java/edu/forty/bits/datastructures/array/BabyNames.java
+++ src/main/java/edu/forty/bits/datastructures/array/BabyNames.java
@@ -1,6 +1,9 @@
 package edu.forty.bits.datastructures.array;
 
+import edu.forty.bits.datastructures.annotations.Array;
+import edu.forty.bits.datastructures.annotations.Graph;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.*;
 import java.util.function.Function;
@@ -14,6 +17,8 @@ import java.util.stream.Collectors;
  * of the true frequency of each name. Note that, the pairs are both transitive and symmetric. In the final
  * list, any name can be used as a real name.
  */
+@Graph
+@Array
 public class BabyNames {
 
     // todo: time-complexity?
@@ -58,4 +63,144 @@ public class BabyNames {
 
     // Another approach would be creating a graph while iterating the synonym pairs
     // and traversing each of its node performing a DFS/BFS to get the values from frequency table
+
+
+    /* Add all names to graph as nodes. */
+    public static Graph constructGraph(Map<String, Integer> names) {
+        Graph graph = new Graph();
+        for (Map.Entry<String, Integer> entry : names.entrySet()) {
+            String name = entry.getKey();
+            int frequency = entry.getValue();
+            graph.createNode(name, frequency);
+        }
+        return graph;
+    }
+
+    /* Connect synonymous spellings. */
+    public static void connectEdges(Graph graph, String[][] synonyms) {
+        for (String[] entry : synonyms) {
+            String name1 = entry[0];
+            String name2 = entry[1];
+            graph.addEdge(name1, name2);
+        }
+    }
+
+    /* Do depth-first search to find the total frequency of this
+     * component, and mark each node as visited.*/
+    public static int getComponentFrequency(GraphNode node) {
+        if (node.isVisited()) {
+            return 0;
+        }
+        node.setVisited(true);
+        int sum = node.getFrequency();
+        for (GraphNode child : node.getNeighbors()) {
+            sum += getComponentFrequency(child);
+        }
+        return sum;
+    }
+
+    /* Do DFS of each component. If a node has been visited before,
+     * then its component has already been computed. */
+    public static Map<String, Integer> getTrueFrequencies(Graph graph) {
+        return graph.getNodes().stream()
+                .filter(node -> !node.isVisited())
+                .collect(Collectors.toMap(GraphNode::getName, BabyNames::getComponentFrequency, (a, b) -> b));
+    }
+
+    public static Map<String, Integer> trulyMostPopular(Map<String, Integer> names, String[][] synonyms) {
+        Graph graph = constructGraph(names);
+        connectEdges(graph, synonyms);
+        return getTrueFrequencies(graph);
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> names = new HashMap<>();
+        names.put("John", 3);
+        names.put("Jonathan", 4);
+        names.put("Johnny", 5);
+        names.put("Chris", 1);
+        names.put("Kris", 3);
+        names.put("Brian", 2);
+        names.put("Bryan", 4);
+        names.put("Carleton", 4);
+
+        String[][] synonyms =
+                {{"John", "Jonathan"},
+                        {"Jonathan", "Johnny"},
+                        {"Chris", "Kris"},
+                        {"Brian", "Bryan"}};
+
+        Map<String, Integer> rootNames = trulyMostPopular(names, synonyms);
+        for (Map.Entry<String, Integer> entry : rootNames.entrySet()) {
+            String name = entry.getKey();
+            int frequency = entry.getValue();
+            System.out.println(name + ": " + frequency);
+        }
+    }
+
+
+    @Getter
+    @Setter
+    static class GraphNode {
+        private List<GraphNode> neighbors;
+        private Map<String, GraphNode> map;
+        private String name;
+        private int frequency;
+        private boolean visited = false;
+
+        public GraphNode(String nm, int freq) {
+            name = nm;
+            frequency = freq;
+            neighbors = new ArrayList<>();
+            map = new HashMap<>();
+        }
+
+        public boolean addNeighbor(GraphNode node) {
+            if (map.containsKey(node.getName())) {
+                return false;
+            }
+            neighbors.add(node);
+            map.put(node.getName(), node);
+            return true;
+        }
+    }
+
+
+    @Getter
+    static class Graph {
+        private List<GraphNode> nodes;
+        private Map<String, GraphNode> map;
+
+        public Graph() {
+            map = new HashMap<>();
+            nodes = new ArrayList<>();
+        }
+
+        public GraphNode createNode(String name, int freq) {
+            if (map.containsKey(name)) {
+                return getNode(name);
+            }
+            GraphNode node = new GraphNode(name, freq);
+            nodes.add(node);
+            map.put(name, node);
+            return node;
+        }
+
+        public boolean hasNode(String name) {
+            return map.containsKey(name);
+        }
+
+        private GraphNode getNode(String name) {
+            return map.get(name);
+        }
+
+        public void addEdge(String startName, String endName) {
+            GraphNode start = getNode(startName);
+            GraphNode end = getNode(endName);
+            if (start != null && end != null) {
+                start.addNeighbor(end);
+                end.addNeighbor(start);
+            }
+        }
+    }
 }
---
+++ src/main/java/edu/forty/bits/datastructures/tree/BabyNames.java
@@ -0,0 +1,7 @@
+package edu.forty.bits.datastructures.tree;
+
+/**
+ * 7,12,13,14,17,20,22,25
+ */
+public class BabyNames {
+}
